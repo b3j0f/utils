1c750df1e19620e8fa40d38869f3b7800b3cedd9
mrbozzo <jlabejof@yahoo.fr>
2014-11-15 22:49:28 +0100

    :add: license header in source files and separate modules in files in documentatio

--- b3j0f/utils/runtime.py
+++ b3j0f/utils/runtime.py
@@ -1,5 +1,29 @@
 # -*- coding: utf-8 -*-
 
+# --------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Jonathan Labéjof <jonathan.labejof@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# --------------------------------------------------------------------
+
 """
 Code from http://code.activestate.com/recipes/277940-decorator-for-\
 bindingconstants-at-compile-time/
@@ -13,8 +37,8 @@ Modifications:
 
 - Add constants values from opmap constants (STORE_GLOBAL, etc.) in order to
     avoid to update globals.
-- Modify verbose argument which is None or use a function with one argument which
-    can be bound to a print function or a logging function.
+- Modify verbose argument which is None or use a function with one argument
+    which can be bound to a print function or a logging function.
 - Set attributes from originary function such as __dict__, __module__, etc.
 """
 
